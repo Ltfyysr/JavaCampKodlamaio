9ba71543f84cd50ed291bbaed256f46343d72f4d
Ltfyysr <lutfiyeyasar38@gmail.com>
2022-09-25 17:00:13 +0300

    variables

---
+++ javaCampKodlamaio/src/week1/variables.java
@@ -0,0 +1,18 @@
+package week1;
+
+public class variables {
+
+	public static void main(String[] args) {
+		int  ogrencisayisi = 12;
+		
+		String mesaj ="Ögrenci sayısı: ";
+		System.out.println(mesaj + ogrencisayisi );
+		System.out.println(mesaj + ogrencisayisi );
+
+		System.out.println("Ögrenci Sayısı: " + ogrencisayisi );
+		System.out.println("Ögrenci Sayısı: " + ogrencisayisi );
+		System.out.println("Ögrenci Sayısı: " + ogrencisayisi );
+
+	}
+
+}
