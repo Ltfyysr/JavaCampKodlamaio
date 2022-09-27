dee21562ee42d9e3ed61ba880298825e80eb07de
Ltfyysr <lutfiyeyasar38@gmail.com>
2022-09-28 00:59:15 +0300

    methods

--- javaCampKodlamaioWeek2/src/methods2/methods2.java
+++ javaCampKodlamaioWeek2/src/methods2/methods2.java
@@ -8,8 +8,7 @@ public class methods2 {
 		System.out.println(yeniMesaj);
 		int sayi = topla(15, 7);
 		System.out.println(sayi);
-		int toplam = topla2(2,3,4);
-		System.out.println(toplam);
+		
 	}
 
 	public static void ekle() {
@@ -28,13 +27,6 @@ public class methods2 {
 		return sayi1 + sayi2;
 	}
 
-	public static int topla2(int... sayilar) {
-		int toplam = 0;
-		for (int sayi : sayilar) {
-			toplam += sayi;
-		}
-		return toplam;
-	}
 
 	public static String sehirVer() {
 		return "Ankara";
